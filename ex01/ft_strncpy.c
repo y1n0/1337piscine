452a8e7364a7b55f95ac44b07c5848b3fe587411
Abdelkoddous El Idrissi <moel-idr@e2r3p2.1337.ma>
2022-08-27 17:33:16 +0100

    ex02 remove main

--- ex01/ft_strncpy.c
+++ ex01/ft_strncpy.c
@@ -9,13 +9,12 @@
 /*   Updated: 2022/08/26 22:12:59 by moel-idr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-#include <stdio.h>
-#include <string.h>
+
 char	*ft_strncpy(char *dest, char *src, unsigned int n)
 {
-	char	*dest_start;
-	unsigned int		i;
-	int		fill_null;
+	char			*dest_start;
+	unsigned int	i;
+	int				fill_null;
 
 	i = 0;
 	fill_null = 0;
@@ -23,35 +22,13 @@ char	*ft_strncpy(char *dest, char *src, unsigned int n)
 	while (i++ < n)
 	{
 		if (*src == '\0' || fill_null == 1)
-		{
 			*dest = '\0';
-		}
 		else
-		{
 			*dest = *src;
-		}
 		src++;
 		dest++;
 	}
-	// if we finished by filling with null, we dont add this
 	if (fill_null == 0)
-	{
 		*dest = '\0';
-	}
 	return (dest_start);
-}
-
-int	main(void)
-{
-	char	a[6] = "hello";
-	char	b[10];
-	char	sa[6] = "hello";
-	char	sb[10];
-
-	ft_strncpy(b, a, 3);
-	printf("a %s\n", a);
-	printf("b %s\n", b);
-	strncpy(sb, sa, 3);
-	printf("a %s\n", sa);
-	printf("b %s\n", sb);
 }
