262f0342819673cd34835991db05c84f74dff9cc
Abdelkoddous El Idrissi <moel-idr@e2r3p2.1337.ma>
2022-08-27 17:31:41 +0100

    ex00 remove main

--- ex00/ft_strcpy.c
+++ ex00/ft_strcpy.c
@@ -9,8 +9,7 @@
 /*   Updated: 2022/08/26 21:52:41 by moel-idr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-#include <stdio.h>
-#include <string.h>
+
 char	*ft_strcpy(char *dest, char *src)
 {
 	char	*dest_start;
@@ -24,23 +23,4 @@ char	*ft_strcpy(char *dest, char *src)
 	}
 	*dest = '\0';
 	return (dest_start);
-}
-
-int	main(void)
-{
-	char a[10] = "hello";
-
-	char b[10];
-
-	char sa[20] = "heeeey\n";
-	char sb[20];
-
-	ft_strcpy(b, a);
-	printf("a %s\n", a);
-	printf("b %s\n", b);
-
-	strcpy(sb, sa);
-
-	printf("a %s\n", sa);
-	printf("b %s\n", sb);
 }
