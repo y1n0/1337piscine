c7ed17504a1b5bda34204699fc5f3cd26c1b441b
Abdelkoddous El Idrissi <moel-idr@e2r3p2.1337.ma>
2022-08-27 17:36:16 +0100

    02 remove main

--- ex02/ft_str_is_alpha.c
+++ ex02/ft_str_is_alpha.c
@@ -19,23 +19,9 @@ int	ft_str_is_alpha(char *str)
 	while (*str != '\0')
 	{
 		c = *str;
-		// if (!(65 <= *str && *str <= 90 && 97 <= *str && *str <= 122))
 		if (!(('a' <= c && c <= 'z') || ('A' <= c && c <= 'Z')))
 			return (0);
 		str++;
 	}
 	return (1);
-}
-
-int	main(void)
-{
-	printf("[empty] %d\n", ft_str_is_alpha(""));
-	printf("AAA %d\n", ft_str_is_alpha("AAA"));
-	printf("aaa %d\n", ft_str_is_alpha("aaa"));
-	printf("123 %d\n", ft_str_is_alpha("123"));
-	printf("aC %d\n", ft_str_is_alpha("aC"));
-	printf("aacc23 %d\n", ft_str_is_alpha("aacc23"));
-	printf("aac c23 %d\n", ft_str_is_alpha("aac c23"));
-	printf("aac-c23 %d\n", ft_str_is_alpha("aac-c23"));
-	printf("(*&*) %d\n", ft_str_is_alpha("(*&*)"));
 }
