1f08f1e8263adb4ee1a3c281a888eef91183694c
Abdelkoddous El Idrissi <moel-idr@e2r3p2.1337.ma>
2022-08-28 16:14:29 +0100

    norm fix

--- ex01/ft_strncpy.c
+++ ex01/ft_strncpy.c
@@ -6,14 +6,14 @@
 /*   By: moel-idr <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/26 22:12:59 by moel-idr          #+#    #+#             */
-/*   Updated: 2022/08/26 22:12:59 by moel-idr         ###   ########.fr       */
+/*   Updated: 2022/08/28 16:13:48 by moel-idr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 char	*ft_strncpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int	i;
-	char *dest_origin;
+	char			*dest_origin;
 
 	i = 0;
 	dest_origin = dest;
