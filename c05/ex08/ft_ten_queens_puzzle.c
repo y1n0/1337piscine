d94a4a14890f721918fcdadd45ad270f960027ea
Abdelkoddous <a.elidrissi@void.fr>
2022-09-01 00:27:59 +0100

    c05 ex08 is_attacked

--- c05/ex08/ft_ten_queens_puzzle.c
+++ c05/ex08/ft_ten_queens_puzzle.c
@@ -1,28 +1,70 @@
 #include <stdio.h>
 
+int is_attacked2(char *board, int x, int y)
+{
+	char row;
+	char col;
+	char smal;
+	int d;
+
+	col = '0';
+	row = *board;
+	while (row)
+	{
+		printf("row: %c col: %c\n", row, col);
+		if ((row == x && row != 'z') || (col == y && row != 'z') )
+		{
+			printf("on same row or col\n");
+			return 1;
+		}
+		smal = x < y ? x : y;
+		d = -(smal - '0');
+		printf("smal: %c, d start: %d, d end: %d\n", smal, d, 10 - (smal - '0'));
+		while ( d < 10 - (smal - '0'))
+		{
+			printf("testing %c, %c \n", x + d, y + d);
+		if (row == x + d && col == y + d)
+		{
+			printf("on up up diagonal\n");
+			return 1;
+		}
+			printf("testing %c, %c \n", x + d, y - d);
+		if (row == x + d && col == y - d)
+		{
+			printf("on up down diagonal\n");
+			return 1;
+		}
+		d++;
+		}
+
+		col++;
+		row = *(++board);
+	}
+	return 0;
+}
+
 int is_attacked(char *board, int x, int y)
 {
 	int row;
 	int col;
 	int d;
-	int furthest;
+	int biggest;
 	row = 0;
 	while (row < 10)
 	{
 		col = *board++;
+		printf("col %c\n", col);
 		if (x == row || y == col)
 			return 1;
-		furthest = x > y ? x : y;
-		d = 10 - 1 - furthest;
-		// you only need to check the adjacent diagonal square
-		while ( d++ < 10  )
-		if (
-			(row == x + d && col == y + d) ||
-			(row == x - d && col == y - d) ||
-			(row == x - d && col == y + d) ||
-			(row == x + d && col == y - d)
-		)
-		return 1;
+		biggest = x > y ? x : y;
+		d = -biggest;
+
+		while ( d++ < 10 - biggest  )
+			if (
+				(row == x + d && col == y + d) ||
+				(row == x - d && col == y + d) )
+			return 1;
+		row++;
 	}
 	return 0;
 }
@@ -36,9 +78,9 @@ int ft_ten_queens_puzzle(void)
 
 int main()
 {
-	int x, y;
-	char board[10] = "------8136";
+	char x, y;
+	char board[] = "zz6zzzzzzz";
 	printf("board: %s\n", board);
 
-	x = 3; y = 2; printf("(x,y)=(%d,%d) => %d\n", x, y, is_attacked(board, x, y));
+	x = '7'; y = '1'; printf("(x,y)=(%c,%c) => %d\n", x, y, is_attacked2(board, x, y));
 }
