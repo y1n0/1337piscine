3cc3956912e0e279baea0e43ef7f8097f3073d46
Abdelkoddous El Idrissi <moel-idr@e2r3p2.1337.ma>
2022-09-03 13:16:06 +0100

    c04 ex01 format

--- c04/ex01/ft_putstr.c
+++ c04/ex01/ft_putstr.c
@@ -1,21 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putstr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: moel-idr <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/03 13:14:52 by moel-idr          #+#    #+#             */
+/*   Updated: 2022/09/03 13:15:49 by moel-idr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include <unistd.h>
 
-int ft_strlen(char *str)
+int	ft_strlen(char *str)
 {
-	char * str_start;
-	
+	char	*str_start;
+
 	str_start = str;
-	while (*str++ != '\0');
-	return str - str_start - 1 ;
+	while (*str++ != '\0')
+		;
+	return (str - str_start - 1);
 }
 
-void ft_putstr(char *str)
+void	ft_putstr(char *str)
 {
 	write(1, str, ft_strlen(str));
 }
 
-int main()
+/*
+int	main(void)
 {
 	ft_putstr("hello");
 }
+*/
