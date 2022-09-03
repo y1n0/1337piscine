f835018b4c1554eedd7215924d56758752bba7a5
Abdelkoddous El Idrissi <moel-idr@e2r3p2.1337.ma>
2022-09-03 13:14:30 +0100

    ex00 - format

--- c04/ex00/ft_strlen.c
+++ c04/ex00/ft_strlen.c
@@ -1,17 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlen.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: moel-idr <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/02 11:11:41 by moel-idr          #+#    #+#             */
+/*   Updated: 2022/09/03 13:14:01 by moel-idr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <stdio.h>
 
-int ft_strlen(char *str)
+int	ft_strlen(char *str)
 {
-	char * str_start;
-	
+	char	*str_start;
+
 	str_start = str;
-	while (*str++ != '\0');
-	return str - str_start - 1 ;
+	while (*str++ != '\0')
+		;
+	return (str - str_start - 1);
 }
 
-int main()
+/*
+int	main(void)
 {
-
-	char a[] = "hello";
+	char	a[]  = "hello";
 	printf("%d\n", ft_strlen(a));
 }
+*/
