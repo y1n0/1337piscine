e2832bd80dd79e755cd721c67b6542dfbaf57aa6
Abdelkoddous El Idrissi <moel-idr@e2r3p2.1337.ma>
2022-09-03 20:25:09 +0100

    c04 ex04 handle smallest int

--- c04/ex04/ft_putnbr_base.c
+++ c04/ex04/ft_putnbr_base.c
@@ -1,5 +1,16 @@
 #include <unistd.h>
+#include <stdio.h>
 
+int ft_strstr(char *haystack, char needle, int end)
+{
+	int i;
+
+	i = -1;
+	while (++i < end)
+		if (haystack[i] == needle)
+			return 1;
+	return 0;
+}
 
 int ft_strlen(char *str)
 {
@@ -10,34 +21,88 @@ int ft_strlen(char *str)
 	return str - str_start - 1 ;
 }
 
-void ft_putnbr(int nb, char *base)
-//void ft_putnbr(int nb)
+void ft_putnbr_unsigned(unsigned int nb, char *base)
 {
 	char c;
-	int base_n;
+	unsigned int base_n;
 
-	base_n = ft_strlen(base);
-	if (nb < 0)
+	base_n = (unsigned) ft_strlen(base);
+	
+	if (nb < base_n)
 	{
-		write(1, "-", 1);
-		ft_putnbr(-nb, base);
-	}
-	else if (nb < base_n)
-	{
-		//c = nb + '0';
 		c = base [ nb ];
 		write(1, &c, 1);
 	}
 	else
 	{
-		//c = (nb % base_n) + '0';
 		c = base[ nb % base_n ];
-		ft_putnbr(nb / base_n, base);
+		ft_putnbr_unsigned(nb / base_n, base);
 		write(1, &c, 1);
 	}
 }
 
+int	ft_validate(char *base)
+{
+	int i;
+
+	if (*base == '\0')
+	{
+		printf("empty base\n");
+		return 0;
+	}
+	
+	i = -1;
+	while (base[++i] != '\0')
+	{
+		if (base[i] == '+' || base[i] == '-')
+		{
+			printf("base has +\n");
+			return 0;
+		}
+		if (ft_strstr(base, base[i], i) == 1)
+			{
+				printf("dup chars\n");
+				return 0;}
+	}
+	if (i < 2){
+			printf("base less than 2\n");
+		return 0;
+	}
+	return 1;
+}
+
+void ft_putnbr(int nb, char *base)
+{
+	unsigned int unb;
+
+	if (ft_validate(base) == 0)
+		return;
+
+	if (nb < 0)
+	{
+		write(1, "-", 1);
+		unb = (unsigned int) -nb;
+	}
+	else
+		unb = (unsigned int) nb;
+
+	ft_putnbr_unsigned(unb, base);
+	// write(1, "\n", 1);
+}
+
 int main() {
-	ft_putnbr(16, "0123456789abcdef");
+	// ft_putnbr(16, "012345");
+	// write(1, "\n", 1);
+	// ft_putnbr(-16, "0123");
+	// write(1, "\n", 1);
+	// printf("hay %d", ft_strstr("helo", 'e', 3));
+	// printf("hay %d", ft_strstr("helo", 'x', 3));
+
+
+	ft_putnbr(-2147483648, "0123456789abcdef");
+	ft_putnbr(2147483647, "0123456789abcdef");
+	ft_putnbr(-2147483648, "0123456789");
+	ft_putnbr(2147483647, "0123456789");
+
 
 }
