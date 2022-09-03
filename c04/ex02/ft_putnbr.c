feda7c07e670eedf4396e8cc69289e67bfa36424
Abdelkoddous El Idrissi <moel-idr@e2r3p2.1337.ma>
2022-09-03 13:24:24 +0100

    c04 ex02 edge case and fornmat

--- c04/ex02/ft_putnbr.c
+++ c04/ex02/ft_putnbr.c
@@ -1,8 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: moel-idr <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/03 13:16:21 by moel-idr          #+#    #+#             */
+/*   Updated: 2022/09/03 13:19:05 by moel-idr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-void ft_putnbr(int nb)
+#include <unistd.h>
+
+void	ft_putnbr(int nb)
 {
-	char c;
-	if (nb < 0)
+	char	c;
+
+	if (nb == -2147483648)
+	{
+		write(1, "-2", 2);
+		ft_putnbr(147483648);
+	}
+	else if (nb < 0)
 	{
 		write(1, "-", 1);
 		ft_putnbr(-nb);
@@ -19,3 +38,19 @@ void ft_putnbr(int nb)
 		write(1, &c, 1);
 	}
 }
+
+/*
+int	main(void)
+{
+	ft_putnbr(-2147483648);
+	write(1, "\n", 1);
+	ft_putnbr(0);
+	write(1, "\n", 1);
+	ft_putnbr(100);
+	write(1, "\n", 1);
+	ft_putnbr(100);
+	write(1, "\n", 1);
+	ft_putnbr(18282903);
+	write(1, "\n", 1);
+}
+*/
