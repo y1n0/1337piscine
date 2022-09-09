5627632d2153daa628d223d05a73fa5e00cb2c0c
Abdelkoddous El Idrissi <moel-idr@e2r3p2.1337.ma>
2022-09-09 11:10:56 +0100

    ex03 fix bus error

--- c04/ex03/ft_atoi.c
+++ c04/ex03/ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: moel-idr <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/03 13:27:00 by moel-idr          #+#    #+#             */
-/*   Updated: 2022/09/03 13:27:02 by moel-idr         ###   ########.fr       */
+/*   Updated: 2022/09/09 10:49:26 by moel-idr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,7 +41,7 @@ char	*ft_char_atoi(char *str)
 		str++;
 	}
 	n_start = str;
-	while (*str <= '9')
+	while (*str && *str <= '9')
 		str++;
 	*str = '\0';
 	if (sign == -1)
@@ -59,9 +59,9 @@ int	ft_strlen(char *str)
 	return (str - str_start - 1);
 }
 
-int	ft_strnbr(char *str, int len)
+unsigned int	ft_strnbr(char *str, int len)
 {
-	int	n;
+	unsigned int	n;
 
 	n = 0;
 	while (len--)
@@ -86,15 +86,74 @@ int	ft_atoi(char *str)
 		str_nbr++;
 	}
 	len = ft_strlen(str_nbr);
-	return (ft_strnbr(str_nbr, len) * sign);
+	return (signed int)(ft_strnbr(str_nbr, len) * sign);
 }
 
 /*
 int	main(void)
 {
-	char	a[] = " ---+---+-1234ab567";
+	// char	a[] = " ---+---+-1234ab567";
 
-	printf("%s\n", ft_char_atoi(a));
-	printf("%d\n", ft_atoi(a));
+	// printf("%s\n", ft_char_atoi(a));
+	// printf("%d\n", a, ft_atoi(a));
+
+	{
+		char a[] = "0";
+		int i = ft_atoi(a);
+		printf("%s: %d\n", a, i);
+	}
+	{
+		char a[] = "15";
+		int i = ft_atoi(a);
+		printf("%s: %d\n", a, i);
+	}
+	{
+		char a[] = "-25";
+		int i = ft_atoi(a);
+		printf("%s: %d\n", a, i);
+	}
+	{
+		char a[] = "12345";
+		int i = ft_atoi(a);
+		printf("%s: %d\n", a, i);
+	}
+	{
+		char a[] = "987654321";
+		int i = ft_atoi(a);
+		printf("%s: %d\n", a, i);
+	}
+	{
+		char a[] = "-34567";
+		int i = ft_atoi(a);
+		printf("%s: %d\n", a, i);
+	}
+	{
+		char a[] = "2147483647";
+		int i = ft_atoi(a);
+		printf("%s: %d\n", a, i);
+	}
+	{
+		char a[] = "-2147483647";
+		int i = ft_atoi(a);
+		printf("%s: %d\n", a, i);
+	}
+	{
+		char a[] = "-2147483648";
+		int i = ft_atoi(a);
+		printf("%s: %d\n", a, i);
+	}
+	{
+		char a[] = "-2147483649";
+		int i = ft_atoi(a);
+		printf("%s: %d\n", a, i);
+	}
+	{
+		char a[] = " +-21455";
+		int i = ft_atoi(a);
+		(void) i;
+		printf("%s: %d\n", a, i);
+	}
+
+	return 0;
 }
 */
