b666bb03e5517eaae96dab5c7f37539aea0c5a8b
Abdelkoddous El Idrissi <moel-idr@e2r3p2.1337.ma>
2022-09-10 12:27:18 +0100

    c07 ex03 format

--- c07/ex03/ft_strjoin.c
+++ c07/ex03/ft_strjoin.c
@@ -1,40 +1,63 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: moel-idr <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/09 10:15:41 by moel-idr          #+#    #+#             */
+/*   Updated: 2022/09/09 10:36:10 by moel-idr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-char *ft_strjoin(int size, char **strs, char *sep)
+#include <stdio.h>
+#include <stdlib.h>
+
+int	ft_strlen(char *str)
 {
-	char *ret;
-	char *ret_head;
-	int siz;
-	int sep_zid;
-	int i;
+	int	i;
 
 	i = 0;
-	sep_siz = 0;
-	while (sep[i])
-	{
-		sep_siz++;
-	}
-	i = 0;
-	siz = 0;
-	while (i < size)
-	{
-		while (strs[i]++)
-			siz++;
-		siz += se
-	}
-
-	ret = (char *) malloc(siz);
-	ret_head = ret;
-	i = 0;
-	while (i < size)
-	{
-		while (str[i])
-		{
-			*ret++ = strs[i]++;
-		}
-		*ret++ = sep;
-	}
-		
-	if (size == 0)
-		return (char *) ma
-
+	while (*str++)
+		i++;
+	return (i);
 }
+
+char	*ft_strjoin(int size, char **strs, char *sep)
+{
+	char	*ret;
+	int		siz;
+	int		sep_siz;
+	int		i;
+
+	sep_siz = ft_strlen(sep);
+	i = -1;
+	siz = 0;
+	while (++i < size)
+		siz += ft_strlen(strs[i]);
+	siz += sep_siz * (size - 1);
+	ret = (char *)malloc(siz + 1);
+	i = -1;
+	while (++i < size)
+	{
+		while (*strs[i])
+			*ret++ = *strs[i]++;
+		while (*sep && i != size - 1)
+			*ret++ = *sep++;
+		sep = sep - sep_siz;
+	}
+	*ret = '\0';
+	return (ret - siz);
+}
+
+/*
+int	main(int argc, char *argv[])
+{
+	char	*joined;
+
+	{
+		joined = ft_strjoin(argc, argv, "||");
+		printf("argc: %d sep: %s result:\n%s\n", argc, "||", joined);
+	}
+}
+*/
