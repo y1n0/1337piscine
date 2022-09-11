04c7b72d1e2b40a68cb4ebea033694541e6a5cbb
Abdelkoddous El Idrissi <moel-idr@e2r3p2.1337.ma>
2022-09-11 19:09:25 +0100

    c09 ex01

---
+++ c09/ex01/includes/ft.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft.h                                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: moel-idr <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/11 15:53:33 by moel-idr          #+#    #+#             */
+/*   Updated: 2022/09/11 15:53:35 by moel-idr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_H
+# define FT_H
+
+void	ft_putchar(char c);
+void	ft_swap(int *a, int *b);
+void	ft_putstr(char *str);
+int		ft_strlen(char *str);
+int		ft_strcmp(char *s1, char *s2);
+#endif
